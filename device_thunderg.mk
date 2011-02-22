297987b5c616123ec7c8134ed01d3eedbf312602
mik_os <kiril.mik.os@gmail.com>
2011-02-22 19:47:30 +0200

    move dhcpcd.conf to system/etc/dhcpcd

--- device_thunderg.mk
+++ device_thunderg.mk
@@ -85,7 +85,7 @@ PRODUCT_COPY_FILES += \
     device/lge/thunderg/files/lib/modules/wireless.ko:system/lib/modules/wireless.ko \
     device/lge/thunderg/files/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     vendor/lge/thunderg/proprietary/etc/wl/nvram.txt:system/etc/wl/nvram.txt \
-    device/lge/thunderg/files/etc/dhcpd/dhcpcd.conf:system/etc/dhcpd/dhcpcd.conf \
+    device/lge/thunderg/files/etc/dhcpd/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
     vendor/lge/thunderg/proprietary/etc/wl/rtecdc.bin:system/etc/wl/rtecdc.bin \
     vendor/lge/thunderg/proprietary/etc/wl/rtecdc-apsta.bin:system/etc/wl/rtecdc-apsta.bin \
     vendor/lge/thunderg/proprietary/etc/wl/rtecdc-mfgtest.bin:system/etc/wl/rtecdc-mfgtest.bin
