85728b1d0d95ae48c4ca854707908b7008e1bf12
mik_os <kiril.mik.os@gmail.com>
2011-03-06 13:51:39 +0200

    Upgraded libaudio.
    Fixed stereo.
    Fixed routing problems while calling.
    Fixed volume in speakerphone mode.

--- libaudio/AudioHardware.h
+++ libaudio/AudioHardware.h
@@ -49,13 +49,13 @@ namespace android {
 #define EQ_MAX_BAND_NUM 12
 
 #define ADRC_ENABLE  0x0001
-#define ADRC_DISABLE 0x0000
+#define ADRC_DISABLE 0xFFFE
 #define EQ_ENABLE    0x0002
-#define EQ_DISABLE   0x0000
+#define EQ_DISABLE   0xFFFD
 #define RX_IIR_ENABLE  0x0004
-#define RX_IIR_DISABLE 0x0000
+#define RX_IIR_DISABLE 0xFFFB
 #define MBADRC_ENABLE  0x0010
-#define MBADRC_DISABLE 0x0000
+#define MBADRC_DISABLE 0xFFEF
 
 #define AGC_ENABLE     0x0001
 #define NS_ENABLE      0x0002
