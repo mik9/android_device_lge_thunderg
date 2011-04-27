a611e19c625b3fb7813089e753ed3820a44ae745
mik_os <kiril.mik.os@gmail.com>
2011-04-27 21:32:56 +0300

    change device name in vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,4 +20,4 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo generic_thunderg-eng
+add_lunch_combo full_thunderg-eng
